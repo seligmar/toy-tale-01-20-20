7389875587e35eb381e23c9ba9bbaaaf21390c7a
github username <github email address>
2020-01-20 11:39:32 +0100

    recreated up basic structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
+let addToy = false
+const createToyForm = document.querySelector('.add-toy-form')
+
+addBtn.addEventListener('click', () => {
+  // hide & seek with the form
+  addToy = !addToy
+  if (addToy) {
+    toyForm.style.display = 'block'
+  } else {
+    toyForm.style.display = 'none'
+  }
+})
