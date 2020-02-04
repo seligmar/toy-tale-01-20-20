57eb2115681ec45256d11d14e21344b18331c8da
github username <github email address>
2020-02-04 19:48:48 +0100

    added PATCH functinality

--- src/index.js
+++ src/index.js
@@ -21,15 +21,40 @@ const initialize = () => {
 }
 
 const showToy = toy => {
+  let toyNew = toy
   let toyCard = document.createElement('div')
   toyCard.className = 'card'
   toyCard.innerHTML = `
-  <h2>${toy.name}</h2>
-  <img src=${toy.image} class="toy-avatar" />
-  <p>4 Likes </p>
-  <button class="like-btn">Like <3</button>
-  `
+  <h2>${toyNew.name}</h2>
+  <img src=${toyNew.image} class="toy-avatar" />
+  <p>${toyNew.likes} Likes </p>`
+  let likeBtn = document.createElement('button')
+  likeBtn.className = 'like-btn'
+  likeBtn.innerText = 'Like <3'
+  likeBtn.addEventListener('click', e => increaseLikes(e, toyNew))
+  toyCard.append(likeBtn)
   toyCollection.append(toyCard)
+}
+
+const increaseLikes = (e, toy) => {
+  let toyLikes = toy.likes
+  let toyId = toy.id
+  return fetch(`http://localhost:3000/toys/${toyId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({ likes: toyLikes + 1 })
+  })
+    .then(resp => resp.json())
+    .then(showLikes(e))
+}
+
+const showLikes = e => {
+  // debugger
+  e.preventDefault()
+  // debugger
 }
 // // if the <script> is at the top of the page, can use
 // // document.addEventListener('DOMContentLoaded', () => {})
